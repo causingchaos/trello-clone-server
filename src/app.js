0c18f4819bd4f4fe722d2355c00b4129fa9bacee
causingchaos <justin.db@outlook.com>
2021-02-15 20:22:10 -0700

    testing

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const configuration = require('@feathersjs/configuration');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 
-require('dotenv').config();
+require('dotenv').config()
 
 const middleware = require('./middleware');
 const services = require('./services');
