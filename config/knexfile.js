7fbcc536357dd6038a3cb5104cbd51f38576c502
causingchaos <justin.db@outlook.com>
2021-02-16 00:00:08 -0700

    updated code

--- config/knexfile.js
+++ config/knexfile.js
@@ -23,6 +23,12 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_DATABASE,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_ACCESS_KEY,
+    }
   }
 };
